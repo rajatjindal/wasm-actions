cca56c6347db931a2d34bb6980e45718ba91c666
Rajat Jindal <rajatjindal83@gmail.com>
2024-07-08 11:49:57 +0530

    use sdk
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- go.mod
+++ go.mod
@@ -2,8 +2,8 @@ module github.com/rajatjindal/wasm-actions
 
 go 1.22.4
 
-require github.com/rajatjindal/wasm-actions-sdk v0.0.0
+replace github.com/rajatjindal/wasm-actions-sdk => ../wasm-actions-sdk
+
+require github.com/rajatjindal/wasm-actions-sdk v0.0.0-00010101000000-000000000000
 
 require github.com/ydnar/wasm-tools-go v0.0.0-20240703204230-31e89a12c28c // indirect
-
-replace github.com/rajatjindal/wasm-actions-sdk => ../wasm-actions-sdk
--- main.go
+++ main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
-	"net/http"
+	"flag"
+	"fmt"
 	"os"
 
-	"github.com/rajatjindal/wasm-actions-sdk/pkg/fs"
-	httpclient "github.com/rajatjindal/wasm-actions-sdk/pkg/http-client"
-	"github.com/rajatjindal/wasm-actions-sdk/pkg/ifmt"
+	"github.com/rajatjindal/wasm-actions-sdk/pkg/archive"
+	"github.com/rajatjindal/wasm-actions-sdk/pkg/downloads"
 )
 
+type Flags struct {
+	Version     string
+	GitHubToken string
+}
+
 func main() {
-	ifmt.Println("environ", os.Environ())
-	response, err := httpclient.Get("https://google.com")
+	flags := &Flags{}
+
+	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flagset.StringVar(&flags.Version, "version", "latest", "version to install")
+	flagset.StringVar(&flags.GitHubToken, "github-token", "GITHUB_TOKEN", "GitHub token to use for downloading")
+	flagset.Parse(os.Args[1:])
+
+	downloadURL := "https://github.com/rajatjindal/kubectl-reverse-proxy/releases/download/v0.3.0/kubectl-reverse-proxy_v0.3.0_darwin_arm64.tar.gz"
+
+	archiveFile, err := downloads.DownloadFile(downloadURL, "download-file")
 	if err != nil {
-		ifmt.Println(err)
-		os.Exit(1)
+		fmt.Println("error when downloading file ", err)
+		return
 	}
 
-	if response.StatusCode != http.StatusOK {
-		ifmt.Printf("expected status code %d, found: %d", http.StatusOK, response.StatusCode)
-		// os.Exit(1)
+	err = archive.ExtractTarGzTo(archiveFile, "/tmp/extracted")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	_ = fs.ListDirs()
+	// find specific version from releases
+	// download the archive
+	// put it inside tools dir
 }
