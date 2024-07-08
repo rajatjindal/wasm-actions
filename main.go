4c58e2af029f25449eccba3a9cbad75b98d6a01f
Rajat Jindal <rajatjindal83@gmail.com>
2024-07-08 13:01:30 +0530

    args not parsing. but why
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/rajatjindal/wasm-actions-sdk/pkg/archive"
 	"github.com/rajatjindal/wasm-actions-sdk/pkg/downloads"
+	"github.com/rajatjindal/wasm-actions-sdk/pkg/platform"
 )
 
 type Flags struct {
@@ -20,23 +22,45 @@ func main() {
 	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	flagset.StringVar(&flags.Version, "version", "latest", "version to install")
 	flagset.StringVar(&flags.GitHubToken, "github-token", "GITHUB_TOKEN", "GitHub token to use for downloading")
-	flagset.Parse(os.Args[1:])
+	if len(os.Args) >= 2 {
+		flagset.Parse(os.Args[1:])
+	}
 
-	downloadURL := "https://github.com/rajatjindal/kubectl-reverse-proxy/releases/download/v0.3.0/kubectl-reverse-proxy_v0.3.0_darwin_arm64.tar.gz"
+	dir, err := os.MkdirTemp("/spin-ghactions-tmp", "")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// tinygo dont support it yet
+	defer os.RemoveAll(dir)
 
-	archiveFile, err := downloads.DownloadFile(downloadURL, "download-file")
+	downloadFile := filepath.Join(dir, "download-file")
+	runtimeOS, runtimeArch, err := platform.GetPlatform()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	downloadURL := fmt.Sprintf("https://github.com/rajatjindal/kubectl-reverse-proxy/releases/download/%s/kubectl-reverse-proxy_%s_%s_%s.tar.gz", flags.Version, flags.Version, runtimeOS, runtimeArch)
+	archiveFile, err := downloads.DownloadFile(downloadURL, downloadFile)
 	if err != nil {
 		fmt.Println("error when downloading file ", err)
 		return
 	}
 
-	err = archive.ExtractTarGzTo(archiveFile, "/tmp/extracted")
+	err = archive.ExtractTarGzTo(archiveFile, "/spin-ghactions-tmp")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = os.Chmod("/spin-ghactions-tmp/kubectl-reverse_proxy", 0755)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	// find specific version from releases
-	// download the archive
+	// download the archive. ensure trigger pass underlying os/platform
 	// put it inside tools dir
 }
